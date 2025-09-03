b3f1db46aa2e42d9baa465346fb56aba4f511274
Abdullah Sanjaya <njay.vana@gmail.com>
2025-09-03 09:34:11 +0000

    please use this logo. put this in a proper folder and file management.

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,18 +3,11 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <div className="text-center cursor-pointer">
-      {/* 
-        Instructions for you:
-        1. Create a 'public' folder in the root of your project if it doesn't exist.
-        2. Upload your logo file into the 'public' folder.
-        3. Replace '/logo.png' below with the path to your logo file (e.g., '/my-logo.svg').
-      */}
       <Image 
-        src="/logo.png" 
+        src="/logo.svg" 
         alt="Dua Collective Logo" 
         width={120} 
-        height={40}
-        className="invert" // This helps make a dark logo visible on a dark background. Remove if your logo is light.
+        height={75}
         priority
       />
     </div>
